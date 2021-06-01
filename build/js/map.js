5fa90e6e64b90b792db7300ae9b26f2abbe9270d
Pavel Ivanov <skyteno69@mail.ru>
2021-06-01 22:39:52 +0300

    01.06.2021: footer

--- build/js/map.js
+++ build/js/map.js
@@ -1,1 +1,1 @@
-function init(){new ymaps.Map("map",{center:[34.84,-111.77],zoom:10},{searchControlProvider:"yandex#search"}).geoObjects.add(new ymaps.Placemark([34.84,-111.77],{balloonContent:"Отель в <strong>Седоне</strong>"},{preset:"islands#circleDotIcon",iconColor:"red"}))}ymaps.ready(init);
+function init(){new ymaps.Map("map",{center:[34.84,-111.77],zoom:6},{searchControlProvider:"yandex#search"}).geoObjects.add(new ymaps.Placemark([34.84,-111.77],{balloonContent:"Отель в <strong>Седоне</strong>"},{preset:"islands#circleDotIcon",iconColor:"red"}))}ymaps.ready(init);
