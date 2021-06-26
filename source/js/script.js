baa7d5b6971939dae1f9601d8c59c25d64e61162
Pavel Ivanov <skyteno69@mail.ru>
2021-06-26 20:12:02 +0300

    end

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,29 @@
 // script.js
+// Переменные
+let button = document.querySelector(".header__button--two");
+let modal = document.querySelector(".modal");
+let closeButton = document.querySelector(".modal__close");
+
+
+modal.classList.add("modal--hidden");
+
+// Функция, которая при нажатии на кнопку "Профиль" открывает модальное окно
+button.addEventListener("click", function () {
+  if (modal.classList.contains("modal--hidden")) {
+    modal.classList.remove("modal--hidden");
+    modal.classList.add("modal--open");
+  } else {
+    modal.classList.add("modal--hidden");
+    modal.classList.remove("modal--open");
+  }
+});
+
+// Функция, которая при нажатии на "Крестик", закрывает модальное окно
+closeButton.addEventListener("click", function () {
+  if (modal.classList.contains("modal--open")) {
+    modal.classList.remove("modal--open");
+    modal.classList.add("modal--hidden");
+  }
+});
+
+
